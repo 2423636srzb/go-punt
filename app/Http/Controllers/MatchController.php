2911bf805ffd34f980a48396b17b3a18484a61e4
SAHIR KHAN <sahirmughall123@gmail.com>
2025-01-18 16:30:31 +0500

    update

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\SportsService;
 use Illuminate\Http\Request;
 
 class MatchController extends Controller
 {
+    protected $sportsService;
+
+    public function __construct(SportsService $sportsService)
+    {
+       
+        $this->sportsService = $sportsService;
+    }
     public function cricketLive($eventId, $sportId)
     {
-        return view('home.match',compact('eventId','sportId'));
+        $match = $this->sportsService->getSpecificSportData($eventId);
+
+        dd($match);
+        if (!$match) {
+            return redirect()->back()->with('error', ' match not found.');
+        }
+
+        return view('home.match',compact('eventId','sportId','match'));
     }
 }
